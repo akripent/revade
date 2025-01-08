53edb24841e890c84cfe5d7ed9b83da51964f10a
n3x4z <davidlaomac@gmail.com>
2025-01-08 19:21:25 +0100

    desc

--- pushstuff.sh
+++ pushstuff.sh
@@ -8,8 +8,4 @@ git add .
 read -p "Give me a commit desc: " desc
 git commit -m desc
 
-echo "PUSHING!!!"
-
-# Your push command (i have an alias)
-shopt -s expand_aliases
-uprevade
+echo "Now git push it yourself!"
