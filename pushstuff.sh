a531b82743f9ca3bf9d0e887f5589697415ca07d
n3x4z <davidlaomac@gmail.com>
2025-01-08 19:18:54 +0100

    desc

---
+++ pushstuff.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# First stuff
+echo Removing token
+./remtoken.sh
+echo Removed token.
+
+git add .
+read -p "Give me a commit desc: " desc
+git commit -m desc
+
+echo "PUSHING!!!"
+
+# Your push command (i have an alias)
+uprevade
